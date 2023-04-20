adf936e31e293c5e24e327c4449472667e54a589
MikaZico36 <mmonteiro03611@gmail.com>
2023-04-20 09:57:48 +0100

    Correção de problemas do código

--- src/main/java/softwareeng/project/FileLocationFrame.java
+++ src/main/java/softwareeng/project/FileLocationFrame.java
@@ -1,15 +1,19 @@
 package softwareeng.project;
 
-// TODO: Delete this Class. All elements must be moved to other classes
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.io.IOException;
-import java.net.URL;
-import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.logging.Level;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class FileLocationFrame extends JFrame {
     private JLabel locationLabel;
@@ -18,41 +22,23 @@ public class FileLocationFrame extends JFrame {
     private JButton okButton;
     private static final Logger LOGGER = Logger.getLogger("FileLocationFrame");
 
+
     public FileLocationFrame() {
-        super("Schedule PLUS");
+        super("Selecionar localização do ficheiro");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-        // Set the look and feel
-        try {
-            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
-            LOGGER.log(Level.SEVERE, "Error starting program", ex);
-        }
-
-        // Set the size of the JFrame
-        setSize(500, 150);
-
-        // Set the layout and add components
-        setLayout(new GridLayout(2, 1));
+        setLayout(new GridLayout(2, 2));
         initComponents();
         layoutComponents();
         addListeners();
+        pack();
         setLocationRelativeTo(null);
-
-        // Add a window listener to dispose the JFrame on close
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                dispose();
-            }
-        });
     }
 
     private void initComponents() {
-        locationLabel = new JLabel("Schedule URL: ", SwingConstants.CENTER);
+        locationLabel = new JLabel("Localização do ficheiro:");
         locationTextField = new JTextField(20);
-        browseButton = new JButton("Search for file locally");
+        browseButton = new JButton("Procurar ficheiro localmente");
         okButton = new JButton("OK");
-        okButton.setBackground(Color.decode("#5cb85c"));
     }
 
     private void layoutComponents() {
@@ -78,81 +64,50 @@ public class FileLocationFrame extends JFrame {
     }
 
     private void okButtonClicked() {
-        String filelocation = locationTextField.getText();
-        switch (getFileExtension(filelocation)) {
-            case "csv":
-                convertCSVToJson(filelocation);
-                break;
-            case "json":
-                convertJsonToCSV(filelocation);
-                break;
-            case "webcal":
-                convertWebcalToCSV(filelocation);
-                break;
-            case "https":
-                convertHttpsToCSV(filelocation);
-                break;
-            default:
-                if (filelocation.isEmpty()) {
-                    JOptionPane.showMessageDialog(this, "No URL inserted! Please try again.");
-                } else {
-                    JOptionPane.showMessageDialog(this, "Unsupported file extension! Please provide a CSV, JSON or Webcal file.");
-                }
-                break;
+        String location = locationTextField.getText();
+        // faça algo com a localização do arquivo aqui
+        JOptionPane.showMessageDialog(this, "Localização do ficheiro selecionada: " + location);
+        //Verifica se a String com o caminho do ficheiro inserido terminar em .csv, então converte-o em JSon
+        if(location.endsWith(".csv")) {
+            CSVToJson csv = new CSVToJson();
+            csv.convertCSVToJson(location);
+        }else if(location.endsWith(".json")) {
+            try {
+                JSonToCSV json = new JSonToCSV(location);
+                json.convertFile();
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(this, "Erro ao converter o arquivo JSON para CSV: " + e.getMessage());
+            }
+        }else if(location.startsWith("webcal")) {
+        	Web web = new Web();
+        	String s = location.replace("webcal", "https");
+        	
+				try {
+					URL url = new URL(s);
+					web.ReadWeb(url);
+					web.URLToCSV(url);
+				} catch (MalformedURLException e) {
+					LOGGER.log(Level.SEVERE, "Exception occurred", e);
+				} catch (IOException e) {
+					LOGGER.log(Level.SEVERE, "Exception occurred", e);
+				}
+        }else if(location.startsWith("https")){
+        	Web web = new Web();
+        	
+				try {
+					URL url = new URL(location);
+					web.ReadWeb(url);
+					web.URLToCSV(url);
+				} catch (MalformedURLException e) {
+					LOGGER.log(Level.SEVERE, "Exception occurred", e);
+				} catch (IOException e) {
+					LOGGER.log(Level.SEVERE, "Exception occurred", e);
+				}
         }
+        dispose();
     }
 
-    private String getFileExtension(String filename) {
-        if (filename == null) {
-            return null;
-        }
-        int extensionIndex = filename.lastIndexOf(".");
-        if (extensionIndex == -1) {
-            return "";
-        }
-        return filename.substring(extensionIndex + 1);
-    }
-
-    private void convertCSVToJson(String filelocation){
-        CSVToJson csv = new CSVToJson();
-        csv.convertCSVToJson(filelocation);
-    }
-
-    private void convertJsonToCSV(String filelocation) {
-        try {
-            JSonToCSV json = new JSonToCSV(filelocation);
-            json.convertFile();
-        } catch (IOException e) {
-            JOptionPane.showMessageDialog(this, "There was an error converting the JSON file to CSV: " + e.getMessage());
-        }
-    }
-
-    private void convertWebcalToCSV(String filelocation) {
-        Web web = new Web();
-        // Makes it possible to have an .ics file
-        String s = filelocation.replace("webcal", "https");
-        try {
-            URL url = new URL(s);
-            web.ReadWeb(url);
-            web.URLToCSV(url);
-        } catch (IOException e) {
-            LOGGER.log(Level.SEVERE, "Exception occurred", e);
-        }
-    }
-
-    private void convertHttpsToCSV(String filelocation) {
-        Web web = new Web();
-        try {
-            URL url = new URL(filelocation);
-            web.ReadWeb(url);
-            web.URLToCSV(url);
-        } catch (IOException e) {
-            LOGGER.log(Level.SEVERE, "Exception occurred", e);
-        }
-    }
-
-
-    // Methods for testing
+    // Métodos para testar
 
     public JTextField getLocationTextField() {
         return locationTextField;
@@ -173,4 +128,6 @@ public class FileLocationFrame extends JFrame {
     public void clickBrowseButton() {
         browseButton.doClick();
     }
+
+
 }
